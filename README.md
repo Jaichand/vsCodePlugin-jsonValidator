6840c1e8e199d0c8916b79634a6ce730f6709156
Jaichand <jaysamriya21@gmail.com>
2024-11-04 12:06:17 +0530

    updated readMe and moved overLap function out

--- README.md
+++ README.md
@@ -1,71 +1,46 @@
 # JSONFormatter README
 
-This is the README for your extension "JSONFormatter". After writing up a brief description, we recommend including the following sections.
+This is code is about VSCode extension
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
-
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+This VSCode extension will be validating the JSON file and add on functionality of validating the JSON data based on hardcode JSON schema.
+ - Go To JSON File
+ - Right Click on Editor, It will show Sifive Test
+ - Selecting Sifive Test will result the JSON File Validation and if JSON Data is same as schema, it will check for the Overlap.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+In case if it's not working on local machine please check the installed version of VSCode engine and replace the same version on package.json file.
 
-For example:
+  "engines": {
+    "vscode": "1.94.2"
+  },
 
-This extension contributes the following settings:
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+## How to compile the extension?
 
-## Known Issues
+Clone this Repo, and go to the Folder
+ ```
+  cd vsCodePlugin-jsonValidator
+ ```
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Once that is done run,
+```
+ npm install
+```
 
-## Release Notes
+Press `CMD + Shift + P` and Select for `Debug: Start Debugging`
+ - it will open a new VSCode Window Know as Extension Development Host
 
-Users appreciate release notes as you update your extension.
+## How to run the extension?
+ 
+## How to run the test?
 
-### 1.0.0
+## Explanation of your test. Are they unit tests or integration tests?
 
-Initial release of ...
+## Explanation of your logic and architecture
 
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+## If you have more time, what do you want to improve?
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -14,12 +14,34 @@ const ajv = new Ajv(); // Create an instance of Ajv
 const validate = ajv.compile(Schema); // Compile the schema
 
 
+function checkOverLap(portDetails: PortDetails) {
+	// Taking out the values of Ports
+	const formattedPortValue = Object.values(portDetails);
+	/**
+	 * Mapping the StartPort => baseAddress
+	 * EndPort => baseAddress + sizeBytes
+	*/
+	const portStartAndEndDetails = formattedPortValue.map((portObj) =>
+		{ return {startPort: portObj.baseAddress, endPort: portObj.baseAddress + portObj.sizeBytes};
+	});
+	/**
+	 * Sorting the StartPort and EndPort based
+	*/
+	const sortedPortsBasedOnStartAndEnd = portStartAndEndDetails.sort((a, b) => a.startPort - b.startPort);
+	/**
+	 * Looping through the sorted Port Array and checking the startPort and endPort, if the intersect, it is a overlap
+	*/
+	for(var i = 0; i < sortedPortsBasedOnStartAndEnd.length - 1; i++) {
+		if(sortedPortsBasedOnStartAndEnd[i].endPort >= sortedPortsBasedOnStartAndEnd[i+1].startPort) {
+			return "OverLap at " + sortedPortsBasedOnStartAndEnd[i+1].startPort + " " +  sortedPortsBasedOnStartAndEnd[i].endPort;
+		}
+ }
+ return "No OverLap";
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "JSONFormatter" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -27,12 +49,8 @@ export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "JSONFormatter.sifiveTest",
     async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("SiFive test Starting");
 
       const editor = vscode.window.activeTextEditor;
-
       const filePath = editor?.document?.uri?.fsPath;
 
       if (filePath) {
@@ -42,39 +60,12 @@ export function activate(context: vscode.ExtensionContext) {
 					const valid = validate(jsonData);
 					if (valid) {
 						vscode.window.showInformationMessage("JSON is valid!");
-						function checkOverLap(portDetails: PortDetails) {
-							// Taking out the values of Ports
-							const formattedPortValue = Object.values(portDetails);
-							/**
-							 * Mapping the StartPort => baseAddress
-							 * EndPort => baseAddress + sizeBytes
-							*/
-							const portStartAndEndDetails = formattedPortValue.map((portObj) => 
-								{ return {startPort: portObj.baseAddress, endPort: portObj.baseAddress + portObj.sizeBytes};
-							});
-							/**
-							 * Sorting the StartPort and EndPort based
-							*/
-							const sortedPortsBasedOnStartAndEnd = portStartAndEndDetails.sort((a, b) => a.startPort - b.startPort);
-							/**
-							 * Looping through the sorted Port Array and checking the startPort and endPort, if the intersect, it is a overlap
-							*/
-							for(var i = 0; i < sortedPortsBasedOnStartAndEnd.length - 1; i++) {
-								if(sortedPortsBasedOnStartAndEnd[i].endPort >= sortedPortsBasedOnStartAndEnd[i+1].startPort) {
-									return "OverLap at " + sortedPortsBasedOnStartAndEnd[i+1].startPort + " " +  sortedPortsBasedOnStartAndEnd[i].endPort;	
-								}
-						 }
-						 return "No OverLap";
-						}
 						vscode.window.showInformationMessage(JSON.stringify(checkOverLap(jsonData)));
 					}
 				} catch (e) {
 					vscode.window.showErrorMessage("JSON File is Invalid " + e);
 				}
       }
-      vscode.window.showInformationMessage(
-        "SiFive test command executed!"
-      );
     }
   );
 
